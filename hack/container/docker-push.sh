b14c924999a97fb099e06361b726a543dff84b77
Francesco Romani <fromani@redhat.com>
2019-05-07 16:26:19 +0200

    travis: automation improvements
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- hack/container/docker-push.sh
+++ hack/container/docker-push.sh
@@ -7,3 +7,4 @@ VERSION="${1:-devel}"
 echo "$QUAY_BOT_PASS" | docker login -u="$QUAY_BOT_USER" --password-stdin quay.io
 docker build -t quay.io/fromani/kubevirt-metrics-collector:$VERSION .
 docker push quay.io/fromani/kubevirt-metrics-collector:$VERSION
+docker logout quay.io
